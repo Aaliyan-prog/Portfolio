1daf7acfa70b2bc09c33151771711a2f88832882
Aaliyan ahmed <my.inventory786@gmail.com>
2025-05-31 08:38:15 +0500

    Index.html routing vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   base: '/Portfolio/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
